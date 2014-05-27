cef38518d201ca0a79ab5230c9f3999f8cf775ee
jamorim <jmsamorim@outlook.pt>
2014-05-27 12:12:18 +0100

    atualização login

--- projetoLP/projetoUB/form_login.vb
+++ projetoLP/projetoUB/form_login.vb
@@ -19,11 +19,12 @@ Public Class form_login
             Dim pass As String = TextBox2.Text
             Dim passbd As String = ""
             Dim permissao As Integer = 0
+            Dim id_utilizador As Integer = 0
 
             pass = getSHA1Hash(pass)
             'connecta a bd'
             Dim connString As String = "server=projetos.epcjc.net; user id=i07351; password=amorim; database=i07351"
-            Dim sqlQuery As String = "SELECT palavrap_sha1, permissao FROM utilizadores WHERE username = '" & nome & "' AND permissao > 0"
+            Dim sqlQuery As String = "SELECT id, palavrap_sha1, permissao FROM utilizadores WHERE username = '" & nome & "' AND permissao > 0"
             Using sqlConn As New MySqlConnection(connString)
                 Using sqlComm As New MySqlCommand()
                     With sqlComm
@@ -36,6 +37,7 @@ Public Class form_login
                         Dim sqlReader As MySqlDataReader = sqlComm.ExecuteReader()
                         While sqlReader.Read()
                             passbd = sqlReader("palavrap_sha1").ToString()
+                            id_utilizador = sqlReader("id").ToInt()
                             If (sqlReader("permissao").ToInt() > 0) Then
                                 permissao = sqlReader("permissao").ToInt()
                             End If
@@ -51,7 +53,7 @@ Public Class form_login
 
             If (passbd = pass) Then
                 '   Session("administrador_id") =
-                '  form_main.Show()
+                form_main.Show()
                 Me.Close()
             Else
                 MsgBox("O nome de utilizador e/ou a palavra-passe são inválidos ou não tem permissão de administrador. pass na bd = " & passbd)
