cd1eea60b908876cd8b0b332b83bd22b36af4153
jamorim <jmsamorim@outlook.pt>
2014-06-03 12:35:10 +0100

    updt new

--- projetoLP/projetoUB/form_gerirgaleria.Designer.vb
+++ projetoLP/projetoUB/form_gerirgaleria.Designer.vb
@@ -22,30 +22,16 @@ Partial Class form_gerirgaleria
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.GaleriaDataGridView = New System.Windows.Forms.DataGridView()
-        CType(Me.GaleriaDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'GaleriaDataGridView
-        '
-        Me.GaleriaDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.GaleriaDataGridView.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.GaleriaDataGridView.Location = New System.Drawing.Point(0, 0)
-        Me.GaleriaDataGridView.Name = "GaleriaDataGridView"
-        Me.GaleriaDataGridView.Size = New System.Drawing.Size(684, 361)
-        Me.GaleriaDataGridView.TabIndex = 1
         '
         'form_gerirgaleria
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(684, 361)
-        Me.Controls.Add(Me.GaleriaDataGridView)
         Me.Name = "form_gerirgaleria"
         Me.Text = "Gerir galeria"
-        CType(Me.GaleriaDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents GaleriaDataGridView As System.Windows.Forms.DataGridView
 End Class
